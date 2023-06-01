45b0480eab8734cb7a636c14566c025dc154ddf8
kirak98 <kirak98@hotmail.co.uk>
2023-06-01 17:44:10 +0200

    added functionality for select

--- src/index.js
+++ src/index.js
@@ -20,3 +20,20 @@ losAngelesTimeElement.innerHTML = losAngelesTime.format("HH:mm:ss [<small>]A[</s
 }
 updateSeconds();
 setInterval(updateSeconds, 1000);
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+let cityTime = moment().tz(cityTimeZone);
+let citiesElement = document.querySelector("#cities");
+citiesElement.innerHTML = `<div class="city">
+<div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("Do MMMM YYYY")} </div>
+    </div>
+<div class="time"> ${cityTime.format("HH:mm:ss")} <small>${cityTime.format("A")}</small></div>
+</div>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
